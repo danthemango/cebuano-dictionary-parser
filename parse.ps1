c996f49109b9ba679842bdadfbf9e6e06b2e5929
Daniel Guenther <Daniel.Guenther@nanaimo.ca>
2025-12-01 15:03:06 -0800

    parse sentences attempt 1

--- parse.ps1
+++ parse.ps1
@@ -1,14 +1,42 @@
-$html = Get-Content '.\pg40074-images.html'
-[xml]$xml = $html
-if ($xml)
-{
-    $res = Select-Xml -Xml $xml -XPath "//div[@class='div1 letter']"
-    return $res | ForEach-Object { $_.Node.ChildNodes | Where-Object class -eq divBody }
 
-    # this provides you with an array of sections for each letter, with child notes for each definition
-    # e.g. $res[0].ChildNodes[15].InnerXml
+$entries = @()
+
+foreach ($node in $res[0].ChildNodes) {
+    # Skip non-entry nodes
+    if ($node.nodeName -ne "#text") {
+        $wordNode = $node.SelectSingleNode(".//b[@id]")
+        if ($wordNode) {
+            $word = $wordNode.InnerText
+            $lang = $wordNode.GetAttribute("lang")
+            $posNode = $node.SelectSingleNode(".//i[1]")
+            $pos = if ($posNode) { $posNode.InnerText } else { "" }
+
+            # Extract definitions and examples
+            $definitions = @()
+            $senseNodes = $node.SelectNodes(".//b[not(@id)]")
+            foreach ($senseNode in $senseNodes) {
+                $senseNum = $senseNode.InnerText
+                # Get text after this sense until next <b>
+                $defText = $senseNode.NextSibling.InnerText
+                $examples = ($node.SelectNodes(".//i[@lang='ceb']") | ForEach-Object { $_.InnerText })
+                $definitions += [PSCustomObject]@{
+                    sense = $senseNum
+                    definition = $defText
+                    examples = $examples
+                }
+            }
+
+            $entries += [PSCustomObject]@{
+                word = $word
+                lang = $lang
+                part_of_speech = $pos
+                definitions = $definitions
+            }
+        }
+    }
 }
-else
-{
-    # throw "could not convert"
-}
+
+$entries
+
+# Convert to JSON
+# $entries | ConvertTo-Json -Depth 5 | Out-File "dictionary.json"
