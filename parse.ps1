272af8149058787ec825126cfe396ae0b97be075
Daniel Guenther <Daniel.Guenther@nanaimo.ca>
2025-12-01 15:19:30 -0800

    parsing attempt 2

--- parse.ps1
+++ parse.ps1
@@ -1,42 +1,72 @@
 
 $entries = @()
 
-foreach ($node in $res[0].ChildNodes) {
-    # Skip non-entry nodes
-    if ($node.nodeName -ne "#text") {
-        $wordNode = $node.SelectSingleNode(".//b[@id]")
-        if ($wordNode) {
-            $word = $wordNode.InnerText
-            $lang = $wordNode.GetAttribute("lang")
-            $posNode = $node.SelectSingleNode(".//i[1]")
-            $pos = if ($posNode) { $posNode.InnerText } else { "" }
+$html = Get-Content '.\pg40074-images.html'
+[xml]$xml = $html
+if ($xml) {
+    $res = Select-Xml -Xml $xml -XPath "//div[@class='div1 letter']"
+    $res = $res | ForEach-Object { $_.Node.ChildNodes | Where-Object class -eq divBody }
 
-            # Extract definitions and examples
-            $definitions = @()
-            $senseNodes = $node.SelectNodes(".//b[not(@id)]")
-            foreach ($senseNode in $senseNodes) {
-                $senseNum = $senseNode.InnerText
-                # Get text after this sense until next <b>
-                $defText = $senseNode.NextSibling.InnerText
-                $examples = ($node.SelectNodes(".//i[@lang='ceb']") | ForEach-Object { $_.InnerText })
-                $definitions += [PSCustomObject]@{
-                    sense = $senseNum
-                    definition = $defText
-                    examples = $examples
+    # this provides you with an array of sections for each letter, with child notes for each definition
+    # e.g. $res[0].ChildNodes[15].InnerXml
+
+    foreach ($node in $res[0].ChildNodes) {
+        if ($node.nodeName -ne "#text") {
+            $wordNode = $node.SelectSingleNode(".//b[@id]")
+            if ($wordNode) {
+                $word = $wordNode.InnerText
+                $lang = $wordNode.GetAttribute("lang")
+                $posNode = $node.SelectSingleNode(".//i[1]")
+                $pos = if ($posNode) { $posNode.InnerText } else { "" }
+
+                # Extract definitions and examples
+                $definitions = @()
+                $senseNodes = $node.SelectNodes(".//b[not(@id)]")
+                foreach ($senseNode in $senseNodes) {
+                    $senseNum = $senseNode.InnerText
+                    $defText = $senseNode.NextSibling.InnerText
+
+                    # Pair examples with translations
+                    $examples = @()
+                    $exampleNodes = $node.SelectNodes(".//i[@lang='ceb']")
+                    foreach ($exNode in $exampleNodes) {
+                        try {
+                            $cebSentence = $exNode.InnerText.Trim()
+                            $engTranslation = $exNode.NextSibling.InnerText.Trim()
+                            $examples += [PSCustomObject]@{
+                                cebuano = $cebSentence
+                                english = $engTranslation
+                            }
+                        }
+                        catch {
+                            Write-Host "Could not parse $($exNode.InnerXml) $_"
+                        }
+                    }
+
+                    $definitions += [PSCustomObject]@{
+                        sense      = $senseNum
+                        definition = $defText
+                        examples   = $examples
+                    }
                 }
-            }
 
-            $entries += [PSCustomObject]@{
-                word = $word
-                lang = $lang
-                part_of_speech = $pos
-                definitions = $definitions
+                $entries += [PSCustomObject]@{
+                    word           = $word
+                    lang           = $lang
+                    part_of_speech = $pos
+                    definitions    = $definitions
+                }
             }
         }
     }
+
+    return $entries
+
+    # Convert to JSON
+    # $entries | ConvertTo-Json -Depth 6 | Out-File "dictionary.json"
+
+
 }
-
-$entries
-
-# Convert to JSON
-# $entries | ConvertTo-Json -Depth 5 | Out-File "dictionary.json"
+else {
+    throw "could not convert"
+}
