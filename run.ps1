59981f9093aac934e043d508c29714dd91d1ec76
Daniel Guenther <danthemango@gmail.com>
2026-04-20 14:03:32 -0700

    redesign run.ps1 with a full pipe
    - this allows me to pipe the results straight from the tokenizer to the output
    - the group-by still blocks output until tokenization is complete

--- run.ps1
+++ run.ps1
@@ -4,24 +4,35 @@ param (
 
 $xml = .\HTML-to-XML.ps1
 
+if (Test-Path ".\out\tokenlist.csv") {
+    Remove-Item ".\out\tokenlist.csv"
+}
+
+if (Test-Path "out\successful-parse.ndjson") {
+    Remove-Item "out\successful-parse.ndjson"
+}
+
+if (Test-Path "out\failed-parse.ndjson") {
+    Remove-Item "out\failed-parse.ndjson"
+}
+
 # tokenize to file
-$tokens = $xml | .\Tokenize.ps1 -Limit $Limit
-$tokens | Export-Csv -NoTypeInformation -Encoding utf8 -Path .\out\tokenlist.csv
+$xml | .\Tokenize.ps1 -Limit $Limit | ForEach-Object {
+    # export tokens to file
+    $_ | Export-Csv -NoTypeInformation -Encoding utf8 -Path ".\out\tokenlist.csv" -Append
+    $_
+} | Group-Object Word | ForEach-Object {
+    .\Parse-WordDef.ps1 -Tokens $_.Group
+} | ForEach-Object {
+    if ($_.ParseOk) {
+        $_ | ConvertTo-Json -Depth 12 -Compress | Add-Content -Encoding UTF8 -Path "out\successful-parse.ndjson"
+    } else {
+        $_ | ConvertTo-Json -Depth 12 -Compress | Add-Content -Encoding UTF8 -Path "out\failed-parse.ndjson"
+    }
+}
 
-# group tokens by word, and try to parse each word
-$parsed = $tokens | Group-Object Word | ForEach-Object { .\Parse-WordDef.ps1 -Tokens $_.Group }
+# $total_num = $parsed.Count
+# $num_success = ($parsed | Where-Object ParseOk -eq $true).Count
+# $perc = [math]::Round(100 * ($num_success / $total_num),1)
+# Write-Output "parsed $num_success / $total_num ($perc)%"
 
-$total_num = $parsed.Count
-$num_success = ($parsed | Where-Object ParseOk -eq $true).Count
-$perc = [math]::Round(100 * ($num_success / $total_num),1)
-Write-Output "parsed $num_success / $total_num ($perc)%"
-
-$parsed |
-    Where-Object ParseOk -eq $true |
-    ConvertTo-Json -Depth 12 |
-    Set-Content -Encoding UTF8 -Path "out\successful-parse.json"
-
-$parsed |
-    Where-Object ParseOk -ne $true |
-    ConvertTo-Json -Depth 12 |
-    Set-Content -Encoding UTF8 -Path "out\failed-parse.json"
