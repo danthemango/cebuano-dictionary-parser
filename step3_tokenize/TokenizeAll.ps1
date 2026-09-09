403b3ded57201b2a273bbb4d1010e301552a0b6f
daniel guenther <danthemango@gmail.com>
2026-09-09 10:55:49 -0700

    alter tokenizeall
    - use foreach piping
    - fix -Force
    - add infile contents to error files

--- step3_tokenize/TokenizeAll.ps1
+++ step3_tokenize/TokenizeAll.ps1
@@ -9,7 +9,8 @@ mkdir -Force $outDir | Out-Null
 $errorDir = "step3_tokenize\errors"
 mkdir -Force $errorDir | Out-Null
 
-foreach ($inFile in Get-ChildItem -Path $inDir -Filter "para_*.xml") {
+Get-ChildItem -Path $inDir -Filter "para_*.xml" | ForEach-Object {
+    $inFile = $_
     $outFile = $inFile.FullName -replace "para_", "tokens_"
     $outFile = $outFile -replace ".xml$", ".csv"
     $outFile = Join-Path -Path $outDir -ChildPath (Split-Path -Leaf $outFile)
@@ -19,16 +20,25 @@ foreach ($inFile in Get-ChildItem -Path $inDir -Filter "para_*.xml") {
     $errorFile = $errorFile -replace ".xml$", ".txt"
     $errorFile = Join-Path -Path $errorDir -ChildPath (Split-Path -Leaf $errorFile)
 
-    if (-Not (Test-Path $outFile)) {
+    if ((-Not (Test-Path $outFile)) -Or $Force) {
         try {
             if (Test-Path $errorFile) {
                 Remove-Item -Path $errorFile -Force
             }
             . $PSScriptRoot\TokenizeFile.ps1 -InFile $inFile | Export-Csv -Path $outFile -NoTypeInformation
         } catch {
+            # delete the outFile if partially created
+            if (Test-Path $outFile) {
+                Remove-Item $outFile
+            }
+
             $errorMessage = "Failed to tokenize $inFile $_"
             $errorMessage += "`nStack Trace:`n$($_.ScriptStackTrace)"
             $errorMessage | Out-File -FilePath $errorFile -Encoding UTF8
+            if (Test-Path $inFile) {
+                $inFileContents = Get-Content $inFile
+                $errorMessage += "`n$inFileContents"
+            }
             Write-Error $errorMessage
         }
     }
