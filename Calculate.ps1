c5eb04e8762e2903f6447373459419fbae32f459
Daniel Guenther <danthemango@gmail.com>
2026-09-14 09:04:13 -0700

    add timings to calculation

--- Calculate.ps1
+++ Calculate.ps1
@@ -1,23 +1,18 @@
-# successfully parsed counts
 $paraCount = (Get-ChildItem step2_split_paras\data).Count
 $tokenCount = (Get-ChildItem step3_tokenize\data).Count
 $parseCount = (Get-ChildItem step4_parse\data).Count
-# now get the error counts in the \error folders:
+
 $tokenErrorCount = (Get-ChildItem step3_tokenize\errors).Count
 $parseErrorCount = (Get-ChildItem step4_parse\errors).Count
 
-# output absolute numbers
 Write-Output "Paragraphs: $paraCount"
 Write-Output "Tokenized: $tokenCount"
 Write-Output "Parsed: $parseCount"
 
-# output absolute error numbers
 Write-Output "Tokenization Errors: $tokenErrorCount"
 Write-Output "Parsing Errors: $parseErrorCount"
 
-# output percentage of tokens successfully tokenized: $tokenCount / ($tokenCount + $tokenErrorCount)
 Write-Output "Tokenization Rate: $(($tokenCount / ($tokenCount + $tokenErrorCount) * 100).ToString('F2'))%"
 Write-Output "Parsing Rate: $(($parseCount / ($parseCount + $parseErrorCount) * 100).ToString('F2'))%"
 
-# output percentage of paragraphs successfully parsed: $parseCount / ($paraCount)
 Write-Output "Paragraph Parsing Rate: $(($parseCount / $paraCount * 100).ToString('F2'))%"
--- RunAll.ps1
+++ RunAll.ps1
@@ -2,6 +2,16 @@ param (
     [switch]$Force
 )
 
-# .\$PSScriptRoot\step3_tokenize\TokenizeAll.ps1 -Force:$Force
-# .\$PSScriptRoot\step4_parse\ParseAll.ps1 -Force:$Force
+$tokSW = [Diagnostics.Stopwatch]::StartNew()
+. $PSScriptRoot\step3_tokenize\TokenizeAll.ps1 -Force:$Force
+$tokSW.Stop()
+
+$parseSW = [Diagnostics.Stopwatch]::StartNew()
+. $PSScriptRoot\step4_parse\ParseAll.ps1 -Force:$Force
+$parseSW.Stop()
+
+
 .\Calculate.ps1 | Set-Content "Calculation.txt"
+Add-Content "Calculation.txt" "Tokenize Time: $($tokSW.Elapsed.ToString())"
+Add-Content "Calculation.txt" "Parse Time: $($parseSW.Elapsed.ToString())"
+
