0daaae21dd60cf31ab70f659f7d442d18b7d2df6
daniel guenther <danthemango@gmail.com>
2026-09-13 23:48:08 -0700

    update calculation

--- Calculate.ps1
+++ Calculate.ps1
@@ -1,13 +1,23 @@
+# successfully parsed counts
 $paraCount = (Get-ChildItem step2_split_paras\data).Count
 $tokenCount = (Get-ChildItem step3_tokenize\data).Count
 $parseCount = (Get-ChildItem step4_parse\data).Count
+# now get the error counts in the \error folders:
+$tokenErrorCount = (Get-ChildItem step3_tokenize\errors).Count
+$parseErrorCount = (Get-ChildItem step4_parse\errors).Count
 
 # output absolute numbers
-# output percentage of $tokenCount / $paraCount
-# output percentage of $parseCount / $tokenCount
-
 Write-Output "Paragraphs: $paraCount"
 Write-Output "Tokenized: $tokenCount"
 Write-Output "Parsed: $parseCount"
-Write-Output "Tokenization Rate: $(($tokenCount / $paraCount * 100).ToString('F2'))%"
-Write-Output "Parsing Rate: $(($parseCount / $tokenCount * 100).ToString('F2'))%"
+
+# output absolute error numbers
+Write-Output "Tokenization Errors: $tokenErrorCount"
+Write-Output "Parsing Errors: $parseErrorCount"
+
+# output percentage of tokens successfully tokenized: $tokenCount / ($tokenCount + $tokenErrorCount)
+Write-Output "Tokenization Rate: $(($tokenCount / ($tokenCount + $tokenErrorCount) * 100).ToString('F2'))%"
+Write-Output "Parsing Rate: $(($parseCount / ($parseCount + $parseErrorCount) * 100).ToString('F2'))%"
+
+# output percentage of paragraphs successfully parsed: $parseCount / ($paraCount)
+Write-Output "Paragraph Parsing Rate: $(($parseCount / $paraCount * 100).ToString('F2'))%"
