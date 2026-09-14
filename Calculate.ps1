93b473d3783b7e0f27e062f4d8b3f704e43d048d
daniel guenther <danthemango@gmail.com>
2026-09-13 23:34:41 -0700

    pipe calculation to file

--- Calculate.ps1
+++ Calculate.ps1
@@ -6,8 +6,8 @@ $parseCount = (Get-ChildItem step4_parse\data).Count
 # output percentage of $tokenCount / $paraCount
 # output percentage of $parseCount / $tokenCount
 
-Write-Host "Paragraphs: $paraCount"
-Write-Host "Tokenized: $tokenCount"
-Write-Host "Parsed: $parseCount"
-Write-Host "Tokenization Rate: $(($tokenCount / $paraCount * 100).ToString('F2'))%"
-Write-Host "Parsing Rate: $(($parseCount / $tokenCount * 100).ToString('F2'))%"
+Write-Output "Paragraphs: $paraCount"
+Write-Output "Tokenized: $tokenCount"
+Write-Output "Parsed: $parseCount"
+Write-Output "Tokenization Rate: $(($tokenCount / $paraCount * 100).ToString('F2'))%"
+Write-Output "Parsing Rate: $(($parseCount / $tokenCount * 100).ToString('F2'))%"
--- RunAll.ps1
+++ RunAll.ps1
@@ -2,6 +2,6 @@ param (
     [switch]$Force
 )
 
-.\$PSScriptRoot\step3_tokenize\TokenizeAll.ps1 -Force:$Force
-.\$PSScriptRoot\step4_parse\ParseAll.ps1 -Force:$Force
-
+# .\$PSScriptRoot\step3_tokenize\TokenizeAll.ps1 -Force:$Force
+# .\$PSScriptRoot\step4_parse\ParseAll.ps1 -Force:$Force
+.\Calculate.ps1 | Set-Content "Calculation.txt"
