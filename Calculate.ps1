3022b7a5a2f3f286f487943dc5372d18eeb4b364
Daniel Guenther <danthemango@gmail.com>
2026-09-14 13:50:36 -0700

    rewrite calculation file

--- Calculate.ps1
+++ Calculate.ps1
@@ -5,14 +5,6 @@ $parseCount = (Get-ChildItem step4_parse\data).Count
 $tokenErrorCount = (Get-ChildItem step3_tokenize\errors).Count
 $parseErrorCount = (Get-ChildItem step4_parse\errors).Count
 
-Write-Output "Paragraphs: $paraCount"
-Write-Output "Tokenized: $tokenCount"
-Write-Output "Parsed: $parseCount"
-
-Write-Output "Tokenization Errors: $tokenErrorCount"
-Write-Output "Parsing Errors: $parseErrorCount"
-
-Write-Output "Tokenization Rate: $(($tokenCount / ($tokenCount + $tokenErrorCount) * 100).ToString('F2'))%"
-Write-Output "Parsing Rate: $(($parseCount / ($parseCount + $parseErrorCount) * 100).ToString('F2'))%"
-
-Write-Output "Paragraph Parsing Rate: $(($parseCount / $paraCount * 100).ToString('F2'))%"
+Write-Output "Tokenization Rate: $tokenCount / $($tokenCount + $tokenErrorCount) $(($tokenCount / ($tokenCount + $tokenErrorCount) * 100).ToString('F2'))%"
+Write-Output "Parsing Rate: $parseCount / $($parseCount + $parseErrorCount) $(($parseCount / ($parseCount + $parseErrorCount) * 100).ToString('F2'))%"
+Write-Output "Total Paragraph Parsing Rate: $parseCount / $paraCount $(($parseCount / $paraCount * 100).ToString('F2'))%"
