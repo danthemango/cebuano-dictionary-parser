b951b23618d856fc8276dae40620ddcc5012001a
Daniel Guenther <danthemango@gmail.com>
2026-09-16 14:08:07 -0700

    fix mislabelled numbers

--- step2_split_paras/fix-typos.ps1
+++ step2_split_paras/fix-typos.ps1
@@ -29,7 +29,6 @@ function Repair-Typos {
 Write-Output "fix typos"
 
 Repair-Typos -Id 3565 -SearchPattern '<i>a</i> protrusion' -Replacement 'a protrusion'
-Repair-Typos -Id 6047 -SearchPattern '<i lang="ceb">1</i>' -Replacement '<b>1</b>'
 Repair-Typos -Id 6600 -SearchPattern '<b>1</b> a' -Replacement '<b>1a</b>'
 Repair-Typos -Id 6742 -SearchPattern '<i lang="ceb">Ihúlug ning suláta, Mail this letter.</i>' -Replacement '<i lang="ceb">Ihúlug ning suláta,</i> Mail this letter.'
 Repair-Typos -Id 6885 -SearchPattern '<i lang="ceb">Hustu na rung ilarga, Now is a good time to leave.</i>' -Replacement '<i lang="ceb">Hustu na rung ilarga,</i> Now is a good time to leave.'
--- step3_tokenize/Tokenize.Functions.ps1
+++ step3_tokenize/Tokenize.Functions.ps1
@@ -445,6 +445,9 @@ function Repair-Typos {
     process {
         # noun and "a", marked as adjective but probably just a typo
         $Token.Content = $Token.Content -replace '<i>[anv]</i>\s*<i>a</i>', '<i>n</i> a'
+        # a mislabelled number (capture)
+        $Token.Content = $Token.Content -replace '<i lang="ceb">(?<num>(?=[a-z0-9]*\d)[a-z0-9]+)</i>', '<b>${num}</b>'
+
         $Token
     }
 }
